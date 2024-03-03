e48eba8351067a3d38e59d5d142539aaf82b52fc
Rold-Bondswell <157935156+Rold-Bondswell@users.noreply.github.com>
2024-03-03 17:10:51 -0600

    I added the jumping mechanic to my character

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 @export var gravity = 1
+@export var jump_power = 1
+
+var active = true # boolean
+var jump_remaining = 100 #10 means double jumps
+var was_jumping = false
 
 func _ready():
 	print("hello world")                     
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
+	
+	if active:
+		#handle jumping
+		if Input.is_action_just_pressed("jump") and jump_remaining > 0:
+			jump_remaining -= 1
+			was_jumping = true
+			velocity.y = - jump_power 
+		
 	move_and_slide()
