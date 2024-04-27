5214d9007958aa92d44fee5f8976b80aaf453699
Rold-Bondswell <157935156+Rold-Bondswell@users.noreply.github.com>
2024-04-27 18:37:34 -0500

    We made the score in the top right and fixed the bug collecting sound

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var world_speed = 250
 @export var moving_enviorment: Node2D
+@export var collectible_pitch_reset_interval = 2000
 
 @onready var collect_sound = $Sounds/CollectSound
 
@@ -11,10 +12,13 @@ var last_platform_position = Vector2.ZERO
 var next_spawn_time = 0
 var score = 0
 var collectible_pitch = 1.0
+var reset_collectible_pitch_time = 0
 func _ready():
 	rng.randomize()
 	
 func _process(delta):
+	if Time.get_ticks_msec() > reset_collectible_pitch_time:
+		collectible_pitch = 1.0
 	#check to see if we need to sp=awn a new platform
 	if Time.get_ticks_msec() > next_spawn_time:
 		_spawn_next_platform() #refactoring
@@ -42,4 +46,5 @@ func add_score(value):
 	collect_sound.set_pitch_scale(collectible_pitch)
 	collect_sound.play()
 	collectible_pitch += 0.1
+	reset_collectible_pitch_time = Time.get_ticks_msec() + collectible_pitch_reset_interval
 	print(score)
